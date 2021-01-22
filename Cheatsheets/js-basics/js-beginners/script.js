b25537d1b1fc628b46d52d17aa7095a163eaa701
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2021-01-22 19:40:53 +0530

    Update script.js

--- Cheatsheets/js-basics/js-beginners/script.js
+++ Cheatsheets/js-basics/js-beginners/script.js
@@ -56,5 +56,57 @@ function lookUpProfile(name,prop)
 // lookUpProfile("Akira Chan","hobbies");
 // lookUpProfile("Akira Chan","hello");
 
-console.log(Math.random());
+// console.log(`Random Number : ${Math.floor(Math.random()*30)}`);
 
+console.log(parseInt("hello"));
+// var a = 2;
+// a== 2 ? a%2 ===0?console.log("a is even") : console.log("a is odd"): console.log("a is not 2");
+
+// let a = 2;
+// let a = 10;
+
+
+//Object.freeze
+
+let add = (x,y) => x+y;
+console.log(add(3,4 ));
+
+arr1 = [1,2,3,4,5,6];
+arr2 = arr1;
+arr1[0] = 0;
+console.log(`Array1 is : ${arr1}`);
+console.log(`Array2 is : ${arr2}`);
+
+// Spread Operator
+
+l1 = [32,464,8587,9789];
+l2 = [...l1];
+l1[0] = 0;
+console.log(`L1 is ${l1}`);
+console.log(`l2 is ${l2}`);
+
+// const LOCAL_FORECAST = {
+// 	today : {min : 75,max : 78},
+// 	tomorrow : {min : 45,max : 50}
+// };
+
+// function getMinTemp(forecast) 
+// {
+// 	const {today : {min : minTemp}} = forecast;
+// 	return minTemp;	
+// }
+
+// console.log(getMinTemp(LOCAL_FORECAST));
+
+const contactsList = {
+	akhila : {number : '24536457',address : {laneNum : 7,houseNum : 9}},
+	john : {number : 'w4684uryr',address : {laneNum : 57,houseNum : 245}}
+};
+
+// function getLaneNum(contactList,name) {
+// 	const {name : {address : {laneNum : num}}} = contactList;
+// 	return num;
+// }
+
+const {akhila : {address : {laneNum : num}}} = contactsList;
+console.log(`Get lane num ${num}`);
